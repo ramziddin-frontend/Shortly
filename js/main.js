3ef95fc7cbb2398b2ad9aafee2ead2b38776a515
ramziddin-frontend <ramziddintursunov82@gmail.com>
2022-12-18 14:44:39 +0500

    Add evets to a form in javascript and css autofill styles

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const elUrlShortener = document.querySelector('.url-shortener');
+const elUrlShortenerForm = document.querySelector('.js-url-shortener-form');
+const elUrlShortenerResults = document.querySelector('.url-shortener__results');
+
+if (elUrlShortenerForm) {
+  elUrlShortenerForm.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+
+    elUrlShortenerResults.classList.add('url-shortener__results--open');
+  });
+}
+
+if (elUrlShortener) {
+  elUrlShortener.addEventListener('click', function (evt) {
+    if (evt.target.matches('.js-copy-short-link-button')) {
+      // Change button text
+      evt.target.textContent = 'Copied!';
+
+      // Change button bgcolor
+      evt.target.classList.add('url-shortener__copy-button--copied');
+
+      // Copy short link to clipboard
+      navigator.clipboard.writeText(evt.target.previousElementSibling.textContent);
+
+      // Reset button text and class after 1 second
+      setTimeout(function () {
+        evt.target.textContent = 'Copy';
+        evt.target.classList.remove('url-shortener__copy-button--copied');
+      }, 1000);
+    }
+  });
+}
